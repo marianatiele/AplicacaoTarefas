7a613461b04d891a1f081d392a8018fbd167ab74
Maria Natiele <106673906+marianatiele@users.noreply.github.com>
2023-12-18 14:33:38 -0300

    Update conection.js

--- db/conection.js
+++ db/conection.js
@@ -4,7 +4,7 @@ async function main(){
     try{
         mongoose.set('strictQuery', true);
 
-        await mongoose.connect('mongodb+srv://damascenonathy:ccRzctE6NKqMHo4q@cluster0.e6lb0xj.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('Link do mongo online');
         console.log('conectado ao MongoDB Online')
     }catch(error){
         console.log(`erro: ${error}`)
